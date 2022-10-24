f9e268949fef38f1ae239d778118cf509c128925
Dev Sapariya <devsapariya94@gmail.com>
2022-10-24 08:41:25 +0530

    some change in D

--- Project/main.c
+++ Project/main.c
@@ -12,77 +12,105 @@ struct trainDetails
     int Acsleeper;
     int distance;
 };
-int i;
-// Array of Struct trainDetails of dynemic size
-struct trainDetails *train;
 
-// Function to add train details
-void addTrain()
+struct train
 {
-       train = (struct trainDetails *)malloc((i+1) * sizeof(struct trainDetails));
-        printf("Enter the train number: ");
-        scanf("%d", &train[i].trainNo);
-        printf("Enter the train name: ");
-        scanf("%s", train[i].trainName);
-        printf("Enter the source: ");
-        scanf("%s", train[i].source);
-        printf("Enter the destination: ");
-        scanf("%s", train[i].destination);
-        printf("Enter the number of seats: ");
-        scanf("%d", &train[i].seats);
-        printf("Enter the number of sleeper seats: ");
-        scanf("%d", &train[i].sleeper);
-        printf("Enter the number of AC sleeper seats: ");
-        scanf("%d", &train[i].Acsleeper);
-        printf("Enter the distance: ");
-        scanf("%d", &train[i].distance);
-        printf("Train Added Successfully\n");
-        i++;
+    struct trainDetails details;
+    struct train *next;
+};
+
+struct train *head = NULL;
+
+void addTrain(struct trainDetails details)
+{
+    struct train *newTrain = (struct train *)malloc(sizeof(struct train));
+    newTrain->details = details;
+    newTrain->next = NULL;
+    if (head == NULL)
+    {
+        head = newTrain;
+    }
+    else
+    {
+        struct train *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newTrain;
+    }
 }
 
-// Function to display train details
 void displayTrain()
 {
-    for (int j = 0; j < i; j++)
+    struct train *temp = head;
+    while (temp != NULL)
     {
-        system("cls");
-        printf("Train number: %d\n", train[j].trainNo);
-        printf("Train name: %s\n", train[j].trainName);
-        printf("Source: %s\n", train[j].source);
-        printf("Destination: %s\n", train[j].destination);
-        printf("Number of seats: %d\n", train[j].seats);
-        printf("Number of sleeper seats: %d\n", train[j].sleeper);
-        printf("Number of AC sleeper seats: %d\n", train[j].Acsleeper);
-        printf("Distance: %d\n", train[j].distance);
+        printf("Train No: %d\n", temp->details.trainNo);
+        printf("Train Name: %s\n", temp->details.trainName);
+        printf("Source: %s\n", temp->details.source);
+        printf("Destination: %s\n", temp->details.destination);
+        printf("Seats: %d\n", temp->details.seats);
+        printf("Sleeper: %d\n", temp->details.sleeper);
+        printf("AC Sleeper: %d\n", temp->details.Acsleeper);
+        printf("Distance: %d\n", temp->details.distance);
+        temp = temp->next;
     }
+}
+
+struct trainDetails getTrainDetails()
+{
+    struct trainDetails details;
+    printf("Enter Train No: ");
+    scanf("%d", &details.trainNo);
+    printf("Enter Train Name: ");
+    scanf("%s", details.trainName);
+    printf("Enter Source: ");
+    scanf("%s", details.source);
+    printf("Enter Destination: ");
+    scanf("%s", details.destination);
+    printf("Enter Seats: ");
+    scanf("%d", &details.seats);
+    printf("Enter Sleeper: ");
+    scanf("%d", &details.sleeper);
+    printf("Enter AC Sleeper: ");
+    scanf("%d", &details.Acsleeper);
+    printf("Enter Distance: ");
+    scanf("%d", &details.distance);
+    return details;
 }
 
 void main()
 {
-    int choice;
     while (1)
     {
-        printf("1. Add Train Details\n");
-        printf("2. Display Train Details\n");
+        int choice;
+        printf("1. Add Train\n");
+        printf("2. Display Train\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice)
         {
         case 1:
-            addTrain();
+        {
+            struct trainDetails details = getTrainDetails();
+            addTrain(details);
             break;
+        }
         case 2:
-
+        {
             displayTrain();
             break;
+        }
         case 3:
-
+        {
             exit(0);
-            break;
+        }
         default:
-
-            printf("Invalid Choice/n");
+        {
+            printf("Invalid Choice\n");
+        }
         }
     }
 }
